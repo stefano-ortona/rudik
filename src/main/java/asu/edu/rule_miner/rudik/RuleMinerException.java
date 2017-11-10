7ba4e2f14a01c592b6e88bffb6515ffc96daebe7
Stefano Ortona <stefano.ortona@gmail.com>
2017-11-10 11:55:50 +0000

    add local package configuration

--- src/main/java/asu/edu/rule_miner/rudik/RuleMinerException.java
+++ src/main/java/asu/edu/rule_miner/rudik/RuleMinerException.java
@@ -4,22 +4,21 @@ import org.slf4j.Logger;
 
 @SuppressWarnings("serial")
 public class RuleMinerException extends RuntimeException {
-	public RuleMinerException(final String message, final Logger logger) {
-		super(message);
-		if (logger != null) {
-			logger.error(message);
-		}
-	}
+  public RuleMinerException(final String message, final Logger logger) {
+    super(message);
+    if (logger != null) {
+      logger.error(message);
+    }
+  }
 
-	public RuleMinerException(final String message, final Throwable cause, 
-			final Logger logger) {
-		super(message, cause);
-		if (logger != null) {
-			logger.error(message);
-		}
-	}
-	
-	public RuleMinerException(final String message, final Throwable cause) {
-		super(message, cause);
-	}
+  public RuleMinerException(final String message, final Throwable cause, final Logger logger) {
+    super(message, cause);
+    if (logger != null) {
+      logger.error(message);
+    }
+  }
+
+  public RuleMinerException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
 }
--- src/main/java/asu/edu/rule_miner/rudik/configuration/ConfigurationFacility.java
+++ src/main/java/asu/edu/rule_miner/rudik/configuration/ConfigurationFacility.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Constructor;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.varia.NullAppender;
@@ -16,66 +17,79 @@ import asu.edu.rule_miner.rudik.sparql.SparqlExecutor;
 
 public class ConfigurationFacility {
 
-	private static Configuration instance;
+  private static Configuration instance;
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(ConfigurationFacility.class.getName());
+  private final static Logger LOGGER = LoggerFactory.getLogger(ConfigurationFacility.class.getName());
 
-	public synchronized static Configuration getConfiguration(){
-		if(instance == null)
-			initialiseConfiguration(Constant.CONF_FILE);
-		return instance;
-	}
+  public synchronized static Configuration getConfiguration() {
+    if (instance == null) {
+      initialiseConfiguration(Constant.CONF_FILE);
+    }
+    return instance;
+  }
 
-	/**
-	 * Set the global configuration file to the input file name
-	 * @param confFileName
-	 */
-	public static void setConfigurationFile(String confFileName){
-		initialiseConfiguration(confFileName);
-	}
+  /**
+   * Set the global configuration file to the input file name
+   * @param confFileName
+   */
+  public static void setConfigurationFile(String confFileName) {
+    initialiseConfiguration(confFileName);
+  }
 
+  private static void initialiseConfiguration(String confFileName) {
+    BasicConfigurator.configure(new NullAppender());
 
-	private static void initialiseConfiguration(String confFileName){
-		BasicConfigurator.configure(new NullAppender());
+    Configuration config = null;
+    String confLogMessage;
+    try {
+      // first try to read explictly set configuration
+      config = new XMLConfiguration(confFileName);
+      confLogMessage = StringUtils.join("Found xml configuration file at ", "'", confFileName, "'.");
+    } catch (final ConfigurationException e) {
+      // if unable, read package configuration
+      try {
+        config = new XMLConfiguration(ConfigurationFacility.class.getResource("Configuration.xml"));
+        confLogMessage = "Did not find a explicit configuration file, using default packagae configuration.";
+      } catch (final Exception e1) {
+        throw new RuleMinerException("Unable to read configuration file at ''" + Constant.CONF_FILE, e1);
+      }
+    }
+    instance = config;
+    // read the logger properties
+    final String logFile = config.getString(Constant.CONF_LOGGER);
+    if (logFile != null) {
+      PropertyConfigurator.configure(logFile);
+    }
+    LOGGER.info(confLogMessage);
+    LOGGER.info("Log4j properties file to be specified at '{}'.", logFile);
+  }
 
-		Configuration config = null;
-		try {
-			config = new XMLConfiguration(confFileName);
-		} catch (ConfigurationException e) {
-			throw new RuleMinerException("Unable to read conf file at ''"+
-					Constant.CONF_FILE, e);
-		}
-		instance = config;
-		//read the logger properties
-		String logFile = config.getString(Constant.CONF_LOGGER);
-		if(logFile != null)
-			PropertyConfigurator.configure(logFile);
-	}
+  public static SparqlExecutor getSparqlExecutor() {
 
-	public static SparqlExecutor getSparqlExecutor(){
+    if (!ConfigurationFacility.getConfiguration().containsKey(Constant.CONF_SPARQL_ENGINE)) {
+      throw new RuleMinerException("Sparql engine parameters not found in the configuration file.", LOGGER);
+    }
 
-		if(!ConfigurationFacility.getConfiguration().containsKey(Constant.CONF_SPARQL_ENGINE))
-			throw new RuleMinerException("Sparql engine parameters not found in the configuration file.", 
-					LOGGER);
+    final Configuration subConf = ConfigurationFacility.getConfiguration().subset(Constant.CONF_SPARQL_ENGINE);
 
-		Configuration subConf = ConfigurationFacility.getConfiguration().subset(Constant.CONF_SPARQL_ENGINE);
+    if (!subConf.containsKey("class")) {
+      throw new RuleMinerException("Need to specify the class implementing the Sparql engine "
+          + "in the configuration file under parameter 'class'.", LOGGER);
+    }
 
-		if(!subConf.containsKey("class"))
-			throw new RuleMinerException("Need to specify the class implementing the Sparql engine "
-					+ "in the configuration file under parameter 'class'.", LOGGER);
+    SparqlExecutor endpoint;
+    try {
+      final Constructor<?> c = Class.forName(subConf.getString("class")).getDeclaredConstructor(Configuration.class);
+      c.setAccessible(true);
+      endpoint = (SparqlExecutor) c.newInstance(new Object[] { subConf });
+    } catch (final Exception e) {
+      throw new RuleMinerException("Error while instantiang the sparql executor enginge.", e, LOGGER);
+    }
+    return endpoint;
+  }
 
-		SparqlExecutor endpoint;
-		try{
-			Constructor<?> c = Class.forName(subConf.getString("class")).
-					getDeclaredConstructor(Configuration.class);
-			c.setAccessible(true);
-			endpoint = (SparqlExecutor) 
-					c.newInstance(new Object[] {subConf});
-		}
-		catch(Exception e){
-			throw new RuleMinerException("Error while instantiang the sparql executor enginge.", e,LOGGER);
-		}
-		return endpoint;
-	}
-	
+  public static void resetConfiguration() {
+    initialiseConfiguration(Constant.CONF_FILE);
+  }
+
 }
